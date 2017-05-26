6401613054e0a2df0a6dc79233af422e5eb0245c
Filipe Fasolin <ffasolin@gmail.com>
2017-05-26 09:49:49 +0100

    Updates requests handlers with blocks

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 function route(handle, pathname) {
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    return handle[pathname]();
   } else {
     console.log("No request handler found for " + pathname);
+    return "404 Not found";
   }
-  console.log("About to route a request for " + pathname);
 }
 
 exports.route = route;
