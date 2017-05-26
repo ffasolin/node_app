7456782e453979a1ab2f04cab6a63fbe51474acc
Filipe Fasolin <ffasolin@gmail.com>
2017-05-26 09:40:25 +0100

    Adds request handler

--- router.js
+++ router.js
@@ -1,4 +1,9 @@
-function route(pathname) {
+function route(handle, pathname) {
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  } else {
+    console.log("No request handler found for " + pathname);
+  }
   console.log("About to route a request for " + pathname);
 }
 
